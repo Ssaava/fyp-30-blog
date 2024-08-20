9d8f4ae8ec4f83c215101891ef7d3ea18bef0951
Black Apple <Ssaava>
2024-08-21 00:14:13 +0300

    set-up first page of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Home from "./pages/Home";
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-center py-8 px-12">Hello Final Year Project</h1>
+    <Home />
     </>
   )
 }
