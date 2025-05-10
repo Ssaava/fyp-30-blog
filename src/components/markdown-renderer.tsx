37e83ac9b401ebb0d4b65a711b057bb49adf21bc
Ssaava <ssavaemma4@gmail.com>
2025-05-11 00:02:26 +0300

    cleaned code

--- src/components/markdown-renderer.tsx
+++ src/components/markdown-renderer.tsx
@@ -1,12 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import vscDarkPlus from "react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus";
-import rehypeRaw from "rehype-raw";
 import MDEditor from "@uiw/react-md-editor";
 import { useTheme } from "next-themes";
 
